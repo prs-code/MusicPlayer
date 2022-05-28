99197147381bb541d04f84b5743d725e93344112
mostafa mousavi <1374.seyedmostafa@gmail.com>
2022-05-28 13:13:39 +0430

    musicplayer files

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name:'age be man bood',
+        path:'assets/musics/Ali Yasini - Age Be Man Bood (320).mp3',
+        artist:'Ali Yasini',
+        cover:'assets/images/Ali Yasini - Age Be Man Bood (320).jpg'
+    },
+    {
+       name:'man asheq cheshat boodam',
+       path:'assets/musics/number2.mp3',
+       artist:'Hamid Sefat',
+       cover:'assets/images/number2.jpg'
+    },
+    {
+        name:'Bezan Baran',
+        path:'assets/musics/number3.mp3',
+        artist:'Eham',
+        cover:'assets/images/number3.jpg'
+    },
+    {
+        name:'Cheshay Rangish',
+        path:'assets/musics/number4.mp3',
+        artist:'ilia',
+        cover:'assets/images/number4.jpg'
+    }
+]
